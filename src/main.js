30a37ca5b0c7e8f36ba9ae3bb19e2eefdcbf7796
LMCMLJ <mjcalvey2@gmail.com>
2016-11-21 17:19:39 +0000

    Adds snake class

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   var screen = document.getElementById("screen").getContext("2d");
-  var snake = [];
   var tick = 0;
+  var snake = new Snake();
 
   function gameLoop(self) {
     tick += 1;
@@ -13,12 +13,23 @@ window.onload = function () {
   }
   gameLoop(this);
 
-  function updatePosition() {}
+  function updatePosition() {
+  }
 
   function draw() {
-    snake.forEach(function(cube) {
+    screen.clearRect(0, 0, 400, 400);
+    snake.position.forEach(function(cube) {
       screen.fillRect(cube[0] * 40, cube[1] * 40, 40, 40);
     });
   }
+
+  function Snake() {
+    this.direction = 'down';
+    this.position = [[1, 1]];
+  }
+
+  Snake.prototype.getHeadPosition = function () {
+    return this.position[0];
+  };
 
 };
