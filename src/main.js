f7bc5d2afb8abe6b668c2cdacba0d57b679e0ccb
LMCMLJ <mjcalvey2@gmail.com>
2016-11-21 15:30:25 +0000

    Adds grid print

--- src/main.js
+++ src/main.js
@@ -1,19 +1,28 @@
 window.onload = function () {
   var screen = document.getElementById("screen").getContext("2d");
-  var x = 0;
-  var y = 0;
+  var snake = [];
   var tick = 0;
+  var vector = 1;
 
   function gameLoop(self) {
     tick += 1;
     screen.clearRect(0, 0, 400, 400);
-    x += 1;
-    y += 1;
-    screen.fillRect(x, y, 40, 40);
+
+    function addRect(x, y) {
+      snake.push([x, y]);
+    }
+
+    snake.forEach(function(item) {
+      screen.fillRect(item[0] * 40, item[1] * 40, 40, 40);
+    });
+
+    addRect(0, 0);
+    addRect(2, 2);
+    addRect(0, 8);
+    addRect(5, 9);
+
     requestAnimationFrame(gameLoop);
   }
   gameLoop(this);
-
-  draw();
 
 };
