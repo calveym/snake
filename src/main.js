a1cdb14b994c78cd3384d377e2608497fac40f21
LMCMLJ <mjcalvey2@gmail.com>
2016-11-21 16:46:12 +0000

    Finished initial structure

--- src/main.js
+++ src/main.js
@@ -2,30 +2,15 @@ window.onload = function () {
   var screen = document.getElementById("screen").getContext("2d");
   var snake = [];
   var tick = 0;
-  var vector = 1;
 
   function gameLoop(self) {
     tick += 1;
-    screen.clearRect(0, 0, 400, 400);
-
-    function addRect(x, y) {
-      snake.push([x, y]);
-    }
-
-    snake.forEach(function(item) {
-      screen.fillRect(item[0] * 40, item[1] * 40, 40, 40);
-    });
-
-    
-
-
-    addRect(0, 0);
-    addRect(2, 2);
-    addRect(0, 8);
-    addRect(5, 9);
-
+    updatePosition();
+    draw();
     requestAnimationFrame(gameLoop);
   }
   gameLoop(this);
-
+  function updatePosition() {}
+  function draw() {}
+  
 };
