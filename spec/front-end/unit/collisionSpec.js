43be82d7a34870aa980d58ede1b4ac4a46dc493b
Elizabeth Wicks <elizabeth.wicks@hotmail.com>
2016-11-22 17:01:26 +0000

    fixes tests and adds plan for week

--- spec/front-end/unit/collisionSpec.js
+++ spec/front-end/unit/collisionSpec.js
@@ -3,9 +3,12 @@ describe("Collision", function () {
 
   });
   describe("#snakeOnSnake", function () {
+
+    spyOn(snake, 'position')
+    snake.position = [[10, 10], [10, 9], [9, 9], [9, 10], [10, 10]]
     it("returns true if duplicate in snake array", function () {
-      snakeArray = [[10, 10], [10, 9], [9, 9], [9, 10], [10, 10]];
-      expect(collision.isSnakeOnSnake(snakeArray)).toEqual(true);
+      collision = new Collision
+      expect(collision.isSnakeOnSnake(snake.position).toEqual(true);
     });
   });
 });
