7c744db1fcf7aa2f055e061bed3308700bed2d79
Royston Henson <rlqhenson@hotmail.com>
2016-11-24 15:00:12 +0000

    testing database

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
-
+var models = require('./models');
 var express = require('express');
 var app = express();
-
+var router = express.Router();
+var Score = require('./models').Score;
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
@@ -15,11 +16,24 @@ app.use(express.static(__dirname + '/src'));
 
 // set the home page route
 app.get('/', function(req, res) {
+  console.log(models)
+  models.Score.findAll().then(function(scores){
+    res.render('index', {
+      scores: scores
+    });
+  });
+});
 
-    // ejs render automatically looks in the views folder
-    res.render('index');
+app.post('/', function(req, res) {
+  models.Score.create({
+    name: req.body.name,
+    score: req.body.score
+  });
+  res.redirect('/');
 });
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
+
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var express = require('express');
+var router = express.Router();
 var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
@@ -34,3 +36,4 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+module.exports = router;
