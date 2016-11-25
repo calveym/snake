71ef7aa3f3f332d91bbb6ab5cb09c707058e83cb
LMCMLJ <mjcalvey2@gmail.com>
2016-11-25 10:00:48 +0000

    Updates views

---
+++ src/food.js
@@ -0,0 +1,40 @@
+var size, canvasSize, screen, collision;
+
+function Food (collis, transferSize, TransferCanvasSize, transferScreen) {
+  collision = collis;
+  this.position = [4, 4];
+  this.score = 1;
+  size = transferSize;
+  canvasSize = TransferCanvasSize;
+  screen = transferScreen;
+  this.foodExists = true;
+}
+
+Food.prototype.randomCoor = function () {
+  this.position[0] = Math.floor(Math.random() * size);
+  this.position[1] = Math.floor(Math.random() * size);
+};
+
+Food.prototype.generateFood = function (snake, tick) {
+  while(collision.isFoodEaten(snake, this, tick)) {
+    this.randomCoor();
+  }
+};
+
+Food.prototype.printFood = function () {
+  this.foodExists = true;
+  screen.fillRect(this.position[0] * size, this.position[1] * size, size, size);
+};
+
+// Food.prototype.make_mouse = function (screen, x,y,size) {
+//   base_image = new Image();
+//   base_image.src = 'img/mice.png'
+//   base_image.onload = function () {
+//     screen.drawImage(base_image, x, y , size, size)
+//   }
+//
+// };
+
+Food.prototype.updateFood = function (snake, tick) {
+  this.generateFood(snake, tick);
+};
---
+++ src/snake.js
@@ -0,0 +1,43 @@
+var size, canvasSize, screen;
+
+function Snake(transferSize, TransferCanvasSize, transferScreen) {
+  size = transferSize;
+  canvasSize = TransferCanvasSize;
+  screen = transferScreen;
+  this.direction = 'down';
+  this.position = [[10, 10],[10,9]];
+}
+
+Snake.prototype.head = function () {
+  return this.position[0];
+};
+
+
+Snake.prototype.move = function(head) {
+  if (this.direction === 'down'){
+    this.position.unshift([head[0], head[1] + 1]);
+  }
+  else if (this.direction === 'up'){
+    this.position.unshift([head[0], head[1] - 1]);
+  }
+  else if (this.direction === 'left'){
+    this.position.unshift([head[0] - 1, head[1]]);
+  }
+  else if (this.direction === 'right'){
+    this.position.unshift([head[0] + 1, head[1]]);
+  }
+};
+
+Snake.prototype.shrink = function (food, tick) {
+  if(tick - food.feedTick >= 10) {
+    this.position.pop();
+  } else if(food.feedTick === undefined) {
+    this.position.pop();
+  }
+};
+
+Snake.prototype.drawSnake = function () {
+  this.position.forEach(function(cube) {
+    screen.fillRect(cube[0] * size, cube[1] * size, size, size);
+  });
+};
