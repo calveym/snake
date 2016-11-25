eada16631c25b63c80761714910abd2919099cb7
Elizabeth Wicks <elizabeth.wicks@hotmail.com>
2016-11-25 15:21:28 +0000

    k

--- server/models/index.js
+++ server/models/index.js
@@ -14,7 +14,7 @@ console.log(config.use_env_variable);
 console.log(process.env);
 var sequelize = new Sequelize(process.env[config.use_env_variable]);
 
-if (process.env[NODE_ENV]=== "production") {
+if (process.env["NODE_ENV"] === "production") {
   console.log("HELLO");
   console.log(config.use_env_variable);
   console.log(process.env[DATABASE_URL]);
